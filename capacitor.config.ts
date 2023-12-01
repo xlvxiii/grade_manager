036e2e52593c0f365a9ffd1c7cf1cbc95fb93a89
xlvxiii <pangestualviansyah@gmail.com>
2023-12-01 16:19:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'grade_manager',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
